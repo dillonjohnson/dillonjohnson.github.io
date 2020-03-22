26410c3fa308bfbce37610b851b1f45ddedaab50
Dillon Johnson <dillonj@DESKTOP-PGMRV2L.localdomain>
2020-03-22 10:23:50 -0400

    Updates

---
+++ precache-manifest.5d2749b5a4419bb299004f00284083e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec1e4a86f20a6064cf0c7821f4dfbc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5ee2de5baa689a61fd2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1d573174539f19675c4",
+    "url": "/static/js/2.38b85fe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38b85fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ee2de5baa689a61fd2",
+    "url": "/static/js/main.5807369c.chunk.js"
+  },
+  {
+    "revision": "400455eb9b0cbb357f96",
+    "url": "/static/js/runtime-main.4303f2af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
